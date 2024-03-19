0c4c8d5b67e644a4e6872d713c4546e39e9b1f43
Nome <almeidagabriel.nunes@gmail.com>
2024-03-19 13:53:33 -0300

    adicionando mudanças com css

---
+++ script.js
@@ -0,0 +1,15 @@
+let xp = 0;
+let age = 32;
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;
+let fighting;
+let monsterHealth;
+let inventory = ["stick"];
+
+//One method for finding specific elements in your HTML is using the querySelector() method. 
+//The querySelector() method takes a CSS selector as an argument and returns the first element that matches that selector. For example, to find the <h1> element in your HTML, you would write:
+//let h1 = document.querySelector("h1");
+const button1  = document.querySelector("#button1"); //is a variable that is not going to be reassigned.
+const button2  = document.querySelector("#button2");
+const button3  = document.querySelector("#button3");
